fa6013c04b324cb7cece3881218d8cb83ba72986
Ashok Krishnamurthy <ashokkrish@gmail.com>
2022-01-04 15:59:39 -0700

    cleaned up code in R folder

--- R/rasterStack.R
+++ R/rasterStack.R
@@ -13,7 +13,7 @@ createRasterStack <- function(selectedCountry, rasterAgg) {
   url <- paste0("https://data.worldpop.org/GIS/Population/Global_2000_2020_1km_UNadj/2020/", inputISO, "/", inputISOLower, "_ppp_2020_1km_Aggregated_UNadj.tif")
   
   tifFileName <- basename(url)    # name of the .tif file
-  tifFolder <- "tif/"             # .tif files should be stored in local tif/ folder
+  tifFolder <- "../tif/"             # .tif files should be stored in local tif/ folder
   
   if (!file.exists(paste0(tifFolder, tifFileName)))
   {
@@ -53,7 +53,7 @@ createRasterStack <- function(selectedCountry, rasterAgg) {
   
   #print(gadmFileName)
   
-  gadmFolder <- "gadm/"         # .rds files should be stored in local gadm/ folder
+  gadmFolder <- "../gadm/"         # .rds files should be stored in local gadm/ folder
   
   # if (file.exists(paste0(gadmFolder, gadmFileName)))
   # {
@@ -116,7 +116,8 @@ createRasterStack <- function(selectedCountry, rasterAgg) {
 #------------------------#
 # Example Function Calls #
 #------------------------#
-
+# #set working directory to source file location if this function is to be tested standalone
+#
 # rs <- createRasterStack("Czech Republic", 10)
 # rs
 # names(rs)
@@ -124,8 +125,6 @@ createRasterStack <- function(selectedCountry, rasterAgg) {
 # rs$rasterStack$Susceptible
 #
 # createRasterStack("Nigeria", 25)
-# 
-# createRasterStack("Belgium", 5)
 # 
 # createRasterStack("Czech Republic", 10)
 # 
