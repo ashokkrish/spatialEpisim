b3d6dc9de25349fe6af0a517ba085dba561f7a8b
Ashok Krishnamurthy <ashokkrish@gmail.com>
2022-02-25 01:34:52 -0700

    minor changes to code

---
+++ WorldPopViz.R
@@ -0,0 +1,61 @@
+library(shiny)
+library(shinyjs)
+library(shinyWidgets)
+library(xlsx)
+
+population <- read.xlsx("misc/population.xlsx", 1)
+source("R/rasterBasePlot.R")
+
+ui <- fluidPage(
+  
+  navbarPage(title = span("2020 UN-Adjusted Population Count Rasters", style = "color:#000000; font-weight:bold; font-size:15pt"),
+             
+             tabPanel(title = "Raster Base Plot",
+                      sidebarLayout(
+                        sidebarPanel(
+
+                          div(
+                            id = "dashboard",
+                            
+                            pickerInput(
+                                 inputId = "selectedCountry",
+                                 choices = population$Country,
+                                 multiple = FALSE,
+                                 select = NULL, #"Nigeria",
+                                 options = pickerOptions(
+                                      actionsBox = TRUE,
+                                      title = "Please select a country"),
+                                 width = "240px"
+                              )
+                            ),
+                        ), 
+                        
+                        mainPanel(
+                          tabsetPanel(id = 'tabSet',
+                                      tabPanel(title = "Raster Base Plot", 
+                                               imageOutput("outputImage")
+                              )
+                         )
+                    )
+               )
+          )
+     )
+)
+
+server <- function(input, output, session){
+  # validate(need(!is.null(input$selectedCountry), "Loading App...")) # catches UI warning
+  # if (!is.null(input$selectedCountry) && input$selectedCountry != ""){
+    output$outputImage <- renderImage({
+      outfile <- tempfile(fileext = '.png')
+      
+      #createBasePlot(input$selectedCountry, 1, FALSE) # print the susceptible plot to www/
+      png(outfile, width = 800, height = 600)
+      createBasePlot(input$selectedCountry, 1, TRUE)  # print the susceptible plot direct to UI
+      dev.off()
+      
+      list(src = outfile, contentType = 'image/png', width = 800, height = 600, alt = "Base plot image not found")
+    }, deleteFile = TRUE)
+  #}
+}
+
+shinyApp(ui,server)
