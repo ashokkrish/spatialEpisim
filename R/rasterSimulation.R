4be28a87f15ca9c3d7079b73b24203684d1466e5
Ashok Krishnamurthy <ashokkrish@gmail.com>
2022-04-08 02:30:43 -0600

    increased mp4 duration to 15 seconds

--- R/rasterSimulation.R
+++ R/rasterSimulation.R
@@ -390,7 +390,7 @@ wtd_nbrs_sum <- function(input_matrix, radius, lambda)
     
   # MERGE THE PNGs TO A GET AN MP4 VIDEO  
   setwd("www/MP4")
-  videoDuration <- 10 # in seconds
+  videoDuration <- 15 # in seconds
   av::av_encode_video(list.files(pattern = ".png"), framerate = timestep/videoDuration, output = paste0(rasterLayer, "_MP4.mp4"))
   setwd("./../..")
   
