1ed2d0983c103e867d30b0341d591a2df1cbab7f
Ashok Krishnamurthy <ashokkrish@gmail.com>
2023-06-27 01:48:01 -0600

    Changes made on June 27, 2023 - Ashok

--- R/rasterStack.R
+++ R/rasterStack.R
@@ -1,14 +1,20 @@
+rm(list = ls())
 library(countrycode)
 library(raster, warn.conflicts=FALSE)
 #library(terra) 
 
-# selectedCountry = "Czech Republic"
-# rasterAgg = 0
-# isCropped = T
-# level1Names = c("Zlínský","Prague") 
+# selectedCountry <- "Nigeria"
+# rasterAgg <- 10
+# isCropped <- T
+# level1Names <- c("Oyo", "Kwara")
+
+# selectedCountry <- "Czech Republic"
+# rasterAgg <- 0
+# isCropped <- T
+# level1Names <- c("Zlínský","Prague") 
 
  createRasterStack <- function(selectedCountry, rasterAgg, isCropped = F, level1Names = NULL) {
-  
+
   #----------------------------------------------------------------#
   # Source 1: WorldPop UN-Adjusted Population Count GeoTIFF raster #
   #----------------------------------------------------------------#
@@ -32,7 +38,7 @@ library(raster, warn.conflicts=FALSE)
   
   WorldPop <- replace(WorldPop, is.na(WorldPop), 0) # Delete this line for clear plot. Check!!!
   
-  # print(WorldPop)
+  print(WorldPop)
   # print(nrow(WorldPop))
   # print(ncol(WorldPop))
   # print(ncell(WorldPop))
@@ -43,44 +49,50 @@ library(raster, warn.conflicts=FALSE)
     Susceptible <- aggregate(WorldPop, fact = c(rasterAgg, rasterAgg), fun = sum, na.rm = TRUE)
   }
   
-  #print(Susceptible)
+  print(Susceptible)
   
+  #---------------------------------------#
+  # Source 2: From GADM: Level1Identifier #
+  #---------------------------------------#
+  
+  gadmFileName <- paste0("gadm36_", inputISO, "_1_sp.rds")  # name of the .rds file
+
+  gadmFolder <- "gadm/"         # .rds files should be stored in local gadm/ folder
+  
+  #print(paste0(gadmFolder, gadmFileName))
+
+  Level1Identifier <- readRDS(paste0(gadmFolder, gadmFileName))
+  
+  print(Level1Identifier)
+  print(Level1Identifier$NAME_1) # List of all states/provinces/regions
+
   if (isCropped)
   {
-    #---------------------------------------#
-    # Source 2: From GADM: Level1Identifier #
-    #---------------------------------------#
+
+    positions <- which(Level1Identifier$NAME_1 %in% level1Names)  # Determines the position of which indices are TRUE.
+    print(positions)
+
+    Level1Identifier <- Level1Identifier[which(Level1Identifier$NAME_1 %in% level1Names), ]
+    print(Level1Identifier) # It is a SpatialPolygonsDataFrame
     
-    gadmFileName <- paste0("gadm36_", inputISO, "_1_sp.rds")  # name of the .rds file
-    
-    #print(gadmFileName)
-    
-    gadmFolder <- "gadm/"         # .rds files should be stored in local gadm/ folder
-    
-    #print(paste0(gadmFolder, gadmFileName))
-    
-    # if (file.exists(paste0(gadmFolder, gadmFileName)))
-    # {
-    Level1Identifier <- readRDS(paste0(gadmFolder, gadmFileName))
+    # Level1Identifier <- Level1Identifier[Level1Identifier$NAME_1 %in% level1Names, ]
     # print(Level1Identifier)
-    Level1Identifier <- Level1Identifier[Level1Identifier$NAME_1 %in% level1Names, ]
-    #print(Level1Identifier)
-    # }
-    # else
-    # {
-    #     Level1Identifier <- getData("GADM", level = 1, country = inputISOLower)
-    # }
-    
-    #print(Level1Identifier$NAME_1 %in% level1Names) # List of all states/provinces/regions
+
+    #print(which(Level1Identifier$NAME_1 %in% level1Names)) # Assigns 1, 2, ...
     #print(table(Level1Identifier$NAME_1 %in% level1Names))
     
     Level1Raster <- crop(Level1Identifier, Susceptible)
-    #print(Level1Raster)
-    
+    print(Level1Raster) # It is still a SpatialPolygonsDataFrame
+
     Level1Raster <- raster(Level1Identifier, resolution = res(Susceptible)[1])
-    Level1Raster <- rasterize(Level1Identifier, Level1Raster)
+    print(Level1Raster) # It is now a RasterLayer
+    print(values(Level1Raster))
     
-    #print(extent(Level1Raster))# May take a few seconds to run
+    Level1Raster <- rasterize(Level1Identifier, Level1Raster)
+    print(Level1Raster) # It is now a RasterLayer
+    print(values(Level1Raster))
+    
+    #print(extent(Level1Raster))
     #Level1Raster <- replace(Level1Raster, is.na(Level1Raster), 0)
     
     #print(table(values(Level1Raster)))
@@ -92,31 +104,77 @@ library(raster, warn.conflicts=FALSE)
     
     # Level1Raster <- round(resample(Level1Raster, Susceptible, method = "bilinear"))
     # Level1Raster <-  round(resample(Level1Raster, Susceptible, method = "ngb", fun ='modal'))
-    values(Level1Raster) <- ifelse(values(Level1Raster) > 0, values(Level1Raster), 0)
-    Level1Raster <-  resample(Level1Raster, Susceptible, method = "ngb")
-    #print(values(Level1Raster))
-    
+
+    Level1Raster <- resample(Level1Raster, Susceptible, method = "ngb")
+
     values(Level1Raster) <- ifelse(values(Level1Raster) > 0, values(Level1Raster), 0) # Refill the rasterLayer with 0, 1, 2, 3, ....
-    
-    #values(Level1Raster) <- ifelse(values(Level1Raster) == 11, values(Level1Raster), 0) # Refill the rasterLayer with 0 and level1Names
-    
-    #print(table(values(Level1Raster)))
-    
-    #values(Level1Raster) <- ifelse(values(Inhabitable) > 0, values(Level1Raster), 0) # Refill the rasterLayer with 0, 1, 2, 3, ....
-    
-    # print(freq(Level1Raster))
-    # 
+    print(table(values(Level1Raster)))
+    print(freq(Level1Raster))
+
     # Level1Raster <- replace(Level1Raster, values(Level1Raster) < 0, 0)
     # Unless you are using method = "ngb" the above line is needed for some countries.
     # The other method is called "bilinear"
     
-    # Below line of code added on May 9, 2022
-    Level1Raster <- replace(Level1Raster, is.na(Level1Raster), 0) 
-   # print(values(Level1Raster))
-    # Background: Aggregating typically an entire column or an entire row or both worth of NAs to the Level1Raster
+    Level1Raster <- replace(Level1Raster, is.na(Level1Raster), 0)
+    print(table(values(Level1Raster)))
+    print(freq(Level1Raster))
+
+    # Background: Aggregating typically an entire column or an entire row or both worth of NAs are added to the Level1Raster
     # NOTE: If rasterAgg = 0 or 1, no NAs are added.
+
+    values(Susceptible) <- ifelse(values(Level1Raster) > 0, values(Susceptible), 0) # Fill the Susceptible Layer with either a 0 or it's actual susceptible.
     
-    # print(table(values(Level1Raster)))
+    Inhabitable <- Vaccinated <- Exposed <- Infected <- Recovered <- Dead <- Susceptible
+    
+    values(Vaccinated) <- values(Exposed) <- values(Infected) <- values(Recovered) <- values(Dead) <- 0 # Fill the entire rasterLayer with zeroes
+    values(Inhabitable) <- ifelse(values(Susceptible) > 0, 1, 0) # Fill the rasterLayer with either a 0 or 1.
+
+    print("isCropped selected")
+    
+  }
+  else
+  {
+    
+    Level1Raster <- crop(Level1Identifier, Susceptible)
+    print(Level1Raster) # It is still a SpatialPolygonsDataFrame
+    
+    Level1Raster <- raster(Level1Identifier, resolution = res(Susceptible)[1])
+    print(Level1Raster) # It is now a RasterLayer
+    print(values(Level1Raster))
+    
+    Level1Raster <- rasterize(Level1Identifier, Level1Raster)
+    print(Level1Raster) # It is now a RasterLayer
+    print(values(Level1Raster))
+    
+    #print(extent(Level1Raster))
+    #Level1Raster <- replace(Level1Raster, is.na(Level1Raster), 0)
+    
+    #print(table(values(Level1Raster)))
+    #(freq(Level1Raster))
+    
+    # Resampling methods
+    # "ngb": Nearest-neighbor; assigns the value of the nearest cell
+    # "bilinear": Bilinear interpolation; assigns a weighted average of the four nearest cells (the default)
+    
+    # Level1Raster <- round(resample(Level1Raster, Susceptible, method = "bilinear"))
+    # Level1Raster <-  round(resample(Level1Raster, Susceptible, method = "ngb", fun ='modal'))
+    
+    Level1Raster <- resample(Level1Raster, Susceptible, method = "ngb")
+    
+    values(Level1Raster) <- ifelse(values(Level1Raster) > 0, values(Level1Raster), 0) # Refill the rasterLayer with 0, 1, 2, 3, ....
+    print(table(values(Level1Raster)))
+    print(freq(Level1Raster))
+    
+    # Level1Raster <- replace(Level1Raster, values(Level1Raster) < 0, 0)
+    # Unless you are using method = "ngb" the above line is needed for some countries.
+    # The other method is called "bilinear"
+    
+    Level1Raster <- replace(Level1Raster, is.na(Level1Raster), 0)
+    print(table(values(Level1Raster)))
+    print(freq(Level1Raster))
+    
+    # Background: Aggregating typically an entire column or an entire row or both worth of NAs are added to the Level1Raster
+    # NOTE: If rasterAgg = 0 or 1, no NAs are added.
     
     values(Susceptible) <- ifelse(values(Level1Raster) > 0, values(Susceptible), 0) # Fill the Susceptible Layer with either a 0 or it's actual susceptible.
     
@@ -124,129 +182,33 @@ library(raster, warn.conflicts=FALSE)
     
     values(Vaccinated) <- values(Exposed) <- values(Infected) <- values(Recovered) <- values(Dead) <- 0 # Fill the entire rasterLayer with zeroes
     values(Inhabitable) <- ifelse(values(Susceptible) > 0, 1, 0) # Fill the rasterLayer with either a 0 or 1.
-    
-    #print(table(as.matrix(Inhabitable)))
-    #print(table(as.matrix(Vaccinated)))
-    
-    print(freq(Level1Raster)) # Frequency table of the values of a RasterLayer.
-    print(freq(Inhabitable))
-    print("isCropped selected")
-    # print(dim(Level1Raster)); print(dim(Susceptible))
-    # print(res(Level1Raster)); print(res(Susceptible))
-    # print(origin(Level1Raster)); print(origin(Susceptible))
-    
-    # print(extent(Level1Raster))
-    # print(extent(Susceptible))
-    
-    #extent(Level1Identifier) <- extent(Susceptible)
-    
-    #print(extent(Level1Identifier))
-    
-    rasterStack <- stack(Susceptible, Vaccinated, Exposed, Infected, Recovered, Dead, Inhabitable, Level1Raster)
-    
-    names(rasterStack) <- c("Susceptible", "Vaccinated", "Exposed", "Infected", "Recovered", "Dead", "Inhabitable", "Level1Raster")
-    
-    #print(Level1Identifier)
-    
-    #print(rasterStack)
-    
-    returnList <- list("rasterStack" = rasterStack, "Level1Identifier" = Level1Identifier, "selectedCountry" = selectedCountry, "rasterAgg" = rasterAgg, "WorldPopRows" = nrow(WorldPop), "WorldPopCols" = ncol(WorldPop), "WorldPopCells" = ncell(WorldPop))
-    
-    return(returnList)
-  }
-  else
-  {
-    Inhabitable <- Vaccinated <- Exposed <- Infected <- Recovered <- Dead <- Susceptible
 
-    values(Vaccinated) <- values(Exposed) <- values(Infected) <- values(Recovered) <- values(Dead) <- 0 # Fill the entire rasterLayer with zeroes
-    values(Inhabitable) <- ifelse(values(Susceptible) > 0, 1, 0) # Fill the rasterLayer with either a 0 or 1.
-
-    #print(table(as.matrix(Inhabitable)))
-    #print(table(as.matrix(Vaccinated)))
-
-    #---------------------------------------#
-    # Source 2: From GADM: Level1Identifier #
-    #---------------------------------------#
-
-    gadmFileName <- paste0("gadm36_", inputISO, "_1_sp.rds")  # name of the .rds file
-
-    #print(gadmFileName)
-
-    gadmFolder <- "gadm/"         # .rds files should be stored in local gadm/ folder
-
-    #print(paste0(gadmFolder, gadmFileName))
-
-    # if (file.exists(paste0(gadmFolder, gadmFileName)))
-    # {
-    Level1Identifier <- readRDS(paste0(gadmFolder, gadmFileName))
-    # }
-    # else
-    # {
-    #     Level1Identifier <- getData("GADM", level = 1, country = inputISOLower)
-    # }
-
-    print(Level1Identifier$NAME_1) # List of all states/provinces/regions
-
-    Level1Raster <- raster(Level1Identifier, resolution = res(Susceptible)[1])
-    Level1Raster <- rasterize(Level1Identifier, Level1Raster) # May take a few seconds to run
-    #Level1Raster <- replace(Level1Raster, is.na(Level1Raster), 0)
-
-    #print(table(values(Level1Raster)))
-    #(freq(Level1Raster))
-
-    # Resampling methods
-    # "ngb": Nearest-neighbor; assigns the value of the nearest cell
-    # "bilinear": Bilinear interpolation; assigns a weighted average of the four nearest cells (the default)
-
-    # Level1Raster <- round(resample(Level1Raster, Susceptible, method = "bilinear"))
-    # Level1Raster <-  round(resample(Level1Raster, Susceptible, method = "ngb", fun ='modal'))
-
-    Level1Raster <-  resample(Level1Raster, Susceptible, method = "ngb")
-
-    values(Level1Raster) <- ifelse(values(Level1Raster) > 0, values(Level1Raster), 0) # Refill the rasterLayer with 0, 1, 2, 3, ....
-
-    #values(Level1Raster) <- ifelse(values(Inhabitable) > 0, values(Level1Raster), 0) # Refill the rasterLayer with 0, 1, 2, 3, ....
-
-    # print(freq(Level1Raster))
-    #
-    # Level1Raster <- replace(Level1Raster, values(Level1Raster) < 0, 0)
-    # Unless you are using method = "ngb" the above line is needed for some countries.
-    # The other method is called "bilinear"
-
-    # Below line of code added on May 9, 2022
-    Level1Raster <- replace(Level1Raster, is.na(Level1Raster), 0)
-    # Background: Aggregating typically an entire column or an entire row or both worth of NAs to the Level1Raster
-    # NOTE: If rasterAgg = 0 or 1, no NAs are added.
-
-    # print(table(values(Level1Raster)))
-
-    print(freq(Level1Raster)) # Frequency table of the values of a RasterLayer.
-    print(freq(Inhabitable))
     print("isCropped not selected")
 
+  }
+
+    #print(table(as.matrix(Inhabitable)))
+    #print(table(as.matrix(Vaccinated)))
+    
+    print(freq(Level1Raster)) # Frequency table of the values of a RasterLayer.
+    print(freq(Inhabitable))
+    
     # print(dim(Level1Raster)); print(dim(Susceptible))
     # print(res(Level1Raster)); print(res(Susceptible))
     # print(origin(Level1Raster)); print(origin(Susceptible))
-
+    
     # print(extent(Level1Raster))
     # print(extent(Susceptible))
-
-    #extent(Level1Identifier) <- extent(Susceptible)
-
-    #print(extent(Level1Identifier))
-
+    
     rasterStack <- stack(Susceptible, Vaccinated, Exposed, Infected, Recovered, Dead, Inhabitable, Level1Raster)
-
+    
     names(rasterStack) <- c("Susceptible", "Vaccinated", "Exposed", "Infected", "Recovered", "Dead", "Inhabitable", "Level1Raster")
-
-    #print(Level1Identifier)
-
-    #print(rasterStack)
-
+    
+    print(rasterStack)
+    
     returnList <- list("rasterStack" = rasterStack, "Level1Identifier" = Level1Identifier, "selectedCountry" = selectedCountry, "rasterAgg" = rasterAgg, "WorldPopRows" = nrow(WorldPop), "WorldPopCols" = ncol(WorldPop), "WorldPopCells" = ncell(WorldPop))
-
+    
     return(returnList)
-}
 }
 
 #------------------------#
@@ -254,6 +216,8 @@ library(raster, warn.conflicts=FALSE)
 #------------------------#
 # #set working directory to source file location if this function is to be tested standalone
 
+# createRasterStack("Nigeria", 0, isCropped = T, level1Names = "Oyo")
+ 
 #createRasterStack("Czech Republic", 0, isCropped = T, level1Names = c("Prague","Zlínský"))
 #createRasterStack("Latvia", 0, isCropped = F, level1Names = NULL)
 #createRasterStack("Nigeria", 0, isCropped = F, level1Names = NULL)
