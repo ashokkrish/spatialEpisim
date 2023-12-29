0ab78f00f854ce47f74f2062d80ffceabd1a135b
Ashok Krishnamurthy <ashokkrish@gmail.com>
2023-12-28 17:22:48 -0700

    minor changes Dec 28 2023

--- WorldPopViz.R
+++ WorldPopViz.R
@@ -13,9 +13,9 @@ source("R/rasterStack.R")
 
 ui <- fluidPage(
   
-  navbarPage(title = span("2020 UN-Adjusted Population Count", style = "color:#000000; font-weight:bold; font-size:15pt"),
+  navbarPage(title = span("WorldPop Visualizer", style = "color:#000000; font-weight:bold; font-size:15pt"),
              
-             tabPanel(title = "Plots and Tables",
+             tabPanel(title = "Plotting a GeoTIFF raster",
                       sidebarLayout(
                         sidebarPanel(
                           div(
@@ -33,9 +33,9 @@ ui <- fluidPage(
                             
                             br(),
 
-                            uiOutput("seedDataButton"),
-
-                            br(),
+                            # uiOutput("seedDataButton"),
+                            # 
+                            # br(),
                             # 
                             # uiOutput("tableButton"),
                           
@@ -56,15 +56,15 @@ ui <- fluidPage(
                         
                         mainPanel(
                           tabsetPanel(id = 'tabSet',
-                                      tabPanel(id = "main", title ="Population Count Map", imageOutput("outputImage"),
+                                      tabPanel(id = "main", title ="2020 UN-Adjusted Population Count Map", imageOutput("outputImage"),
                                                #downloadButton('downloadPlot', 'Save Image')
                                                ),
                                       tabPanel(title ="Selected State/Province Map", imageOutput("croppedOutputImage"),
 
                                                ),
-                                      tabPanel(title ="Population Count by State/Province",
-                                               DT::dataTableOutput("aggTable")
-                                               )
+                                      # tabPanel(title ="Population Count by State/Province",
+                                      #          DT::dataTableOutput("aggTable")
+                                      #          )
                          )
                     ) #mainPanel
                ) # sidebarLayout
@@ -129,84 +129,84 @@ server <- function(input, output, session){
     }
   }, deleteFile = TRUE)
   
-  output$downloadPlot <- downloadHandler(
-    isoCode <- countrycode(input$selectedCountry, origin = "country.name", destination = "iso3c"),
-    filename = sprintf("%s_2020PopulationCount.png", isoCode),
-    content = function(outfile) {
-      if (input$selectedCountry != ""){
-        png(outfile, width = 800, height = 600)
-        #png(outfile, width = 1024, height = 768)
-        createBasePlot(input$selectedCountry, 1, TRUE)
-        dev.off()
-      }
-    })
+  # output$downloadPlot <- downloadHandler(
+  #   isoCode <- countrycode(input$selectedCountry, origin = "country.name", destination = "iso3c"),
+  #   filename = sprintf("%s_2020PopulationCount.png", isoCode),
+  #   content = function(outfile) {
+  #     if (input$selectedCountry != ""){
+  #       png(outfile, width = 800, height = 600)
+  #       #png(outfile, width = 1024, height = 768)
+  #       createBasePlot(input$selectedCountry, 1, TRUE)
+  #       dev.off()
+  #     }
+  #   })
   
   ######################################################################################    
   # Create a table of population count stratified by states/provinces and output to UI #
   ###################################################################################### 
   
-  output$tableButton <- renderUI({
-    validate(need(!is.null(input$selectedCountry), "Loading App...")) # catches UI warning
-    
-    if (!is.null(input$selectedCountry) && input$selectedCountry != ""){
-      actionButton("table","Population Count Table", 
-                   style = "color: #fff; background-color: #337ab7; border-color: #2e6da4", 
-                   style = "length:800px")
-    }
-  })
+  # output$tableButton <- renderUI({
+  #   validate(need(!is.null(input$selectedCountry), "Loading App...")) # catches UI warning
+  #   
+  #   if (!is.null(input$selectedCountry) && input$selectedCountry != ""){
+  #     actionButton("table","Population Count Table", 
+  #                  style = "color: #fff; background-color: #337ab7; border-color: #2e6da4", 
+  #                  style = "length:800px")
+  #   }
+  # })
   
-  observeEvent(input$table, {
-    isCropped <- FALSE
-    
-    if(input$clipLev1 == TRUE)
-    {
-      isCropped <- TRUE
-    }
-    else
-    {
-      isCropped <- FALSE
-    }
-
-    rs <- createRasterStack(input$selectedCountry, 0, isCropped)
-    sus <- rs$rasterStack$Susceptible
-    lvOne <- rs$rasterStack$Level1Raster
-    names <- rs$Level1Identifier$NAME_1
-    #print(rs$rasterStack)
-    #print(sus)
-    #print(lvOne)
-    #print(names)
-    #popCount <- crosstab(sus,lvOne)
-    #print(popCount)
-    
-    lvMatrix <- as.matrix(lvOne)
-    susMatrix <- as.matrix(sus)
-    nMatrix <- as.matrix(names)
-    
-    sumMatrix <- round(aggregate(c(susMatrix) ~ c(lvMatrix), FUN = sum))
-    nameFrame <- data.frame(nMatrix)
-    tableFrame <- data.frame(sumMatrix)
-    testFrame <- data.frame(sumMatrix)
-    #tableFrame <- tail(tableFrame, -1)
-    tableFrame <- tableFrame %>% slice(-1)
-    colnames(tableFrame) <- c("Nums", "Values") # renaming Columns to make it easier to reference them
-    colnames(nameFrame) <- c("Names")
-    tableFrame$Nums <- nameFrame$Names
-    colnames(tableFrame) <- c("State/Province", "Population Count") # Changing Names so it matches to Spec
-    #print(nameFrame)
-    #print(rsMatrix)
-    #print(testFrame)
-    
-    output$aggTable = DT::renderDataTable({DT::datatable(tableFrame,
-                                                         options = list(paging = FALSE,
-                                                                        pageLength = nrow(tableFrame),
-                                                                        #autoWidth = TRUE,
-                                                                        #scrollX = TRUE,
-                                                                        columnDefs = list(list(width = '20%', targets = "_all"))
-                                                         ),
-                                                         selection = 'single',
-                                                         rownames = FALSE
-    )})
-  })
+  # observeEvent(input$table, {
+  #   isCropped <- FALSE
+  #   
+  #   if(input$clipLev1 == TRUE)
+  #   {
+  #     isCropped <- TRUE
+  #   }
+  #   else
+  #   {
+  #     isCropped <- FALSE
+  #   }
+  # 
+  #   rs <- createRasterStack(input$selectedCountry, 0, isCropped)
+  #   sus <- rs$rasterStack$Susceptible
+  #   lvOne <- rs$rasterStack$Level1Raster
+  #   names <- rs$Level1Identifier$NAME_1
+  #   #print(rs$rasterStack)
+  #   #print(sus)
+  #   #print(lvOne)
+  #   #print(names)
+  #   #popCount <- crosstab(sus,lvOne)
+  #   #print(popCount)
+  #   
+  #   lvMatrix <- as.matrix(lvOne)
+  #   susMatrix <- as.matrix(sus)
+  #   nMatrix <- as.matrix(names)
+  #   
+  #   sumMatrix <- round(aggregate(c(susMatrix) ~ c(lvMatrix), FUN = sum))
+  #   nameFrame <- data.frame(nMatrix)
+  #   tableFrame <- data.frame(sumMatrix)
+  #   testFrame <- data.frame(sumMatrix)
+  #   #tableFrame <- tail(tableFrame, -1)
+  #   tableFrame <- tableFrame %>% slice(-1)
+  #   colnames(tableFrame) <- c("Nums", "Values") # renaming Columns to make it easier to reference them
+  #   colnames(nameFrame) <- c("Names")
+  #   tableFrame$Nums <- nameFrame$Names
+  #   colnames(tableFrame) <- c("State/Province", "Population Count") # Changing Names so it matches to Spec
+  #   #print(nameFrame)
+  #   #print(rsMatrix)
+  #   #print(testFrame)
+  #   
+  #   # output$aggTable = DT::renderDataTable({DT::datatable(tableFrame,
+  #   #                                                      options = list(paging = FALSE,
+  #   #                                                                     pageLength = nrow(tableFrame),
+  #   #                                                                     #autoWidth = TRUE,
+  #   #                                                                     #scrollX = TRUE,
+  #   #                                                                     columnDefs = list(list(width = '20%', targets = "_all"))
+  #   #                                                      ),
+  #   #                                                      selection = 'single',
+  #   #                                                      rownames = FALSE
+  #   # )})
+  # })
   
   ################################################    
   # Create select box for choosing input country #
@@ -278,67 +278,67 @@ server <- function(input, output, session){
   # Generate seed data and have an option to download the file locally       #
   ############################################################################ 
   
-  output$seedDataButton <- renderUI({
-    validate(need(!is.null(input$selectedCountry), "Loading App...")) # catches UI warning
-    
-    if (!is.null(input$selectedCountry) && input$selectedCountry != ""){
-      downloadButton('downloadData', "Generate Seed Data", 
-                     style = "color: #fff; background-color: #337ab7; border-color: #2e6da4",
-                     style = "length:800px")
-    }
-  })
+  # output$seedDataButton <- renderUI({
+  #   validate(need(!is.null(input$selectedCountry), "Loading App...")) # catches UI warning
+  #   
+  #   if (!is.null(input$selectedCountry) && input$selectedCountry != ""){
+  #     downloadButton('downloadData', "Generate Seed Data", 
+  #                    style = "color: #fff; background-color: #337ab7; border-color: #2e6da4",
+  #                    style = "length:800px")
+  #   }
+  # })
   
-  observeEvent(input$selectedCountry, {
-    validate(need(!is.null(input$selectedCountry), "Loading App...")) # catches UI warning
-    
-    if (!is.null(input$selectedCountry) && input$selectedCountry != ""){
-      
-      inputISO <- countrycode(input$selectedCountry, origin = 'country.name', destination = 'iso3c') # Converts country name to ISO Alpha
-      
-      gadmFileName <- paste0("gadm36_", inputISO, "_1_sp.rds")  # name of the .rds file
-
-      gadmFolder <- "gadm/" # .rds files should be stored in local gadm/ folder
-
-      # if (file.exists(paste0(gadmFolder, gadmFileName)))
-      # {
-          Level1Identifier <- readRDS(paste0(gadmFolder, gadmFileName))
-      # }
-      # else
-      # {
-      #   Level1Identifier <- getData("GADM", level = 1, country = inputISOLower)
-      # }
-      #print(coordinates(Level1Identifier)) # coords of the region
-      #print(Level1Identifier$NAME_1) # List of all states/provinces/regions
-      
-      seedNames <- Level1Identifier$NAME_1
-      seedCoords <- coordinates(Level1Identifier)
-      #print(seedCoords)
-      seedVaxx <- c(0)
-      seedExpo <- c(0)
-      seedInfect <- c(0)
-      seedRec <- c(0)
-      seedDead <- c(0)
-      seedCombine <- cbind(seedNames, seedCoords, seedVaxx, seedExpo, seedInfect, seedRec, seedDead)
-      frameCombine <- data.frame(seedCombine)
-
-      frameCombine <- frameCombine[c("seedNames", "V3", "V2", "seedVaxx", "seedExpo", "seedInfect", "seedRec", "seedDead")]
-      
-      colnames(frameCombine) <- c("Location", "lat", "lon", "InitialVaccinated", "InitialExposed", "InitialInfections", "InitialRecovered", "InitialDead")
-      #print(frameCombine)
-      
-      isoCode <- countrycode(input$selectedCountry, origin = "country.name", destination = "iso3c")
-      sheetName <- sprintf("%s_initialSeedData", isoCode)
-      
-      output$downloadData <- downloadHandler(
-        filename = function() {
-          paste(sheetName, Sys.Date(), ".csv",  sep = "")
-        },
-        content = function(sheetName) {
-          write.csv(frameCombine, sheetName, row.names = FALSE)
-        }
-      )
-    }
-  })
+  # observeEvent(input$selectedCountry, {
+  #   validate(need(!is.null(input$selectedCountry), "Loading App...")) # catches UI warning
+  #   
+  #   if (!is.null(input$selectedCountry) && input$selectedCountry != ""){
+  #     
+  #     inputISO <- countrycode(input$selectedCountry, origin = 'country.name', destination = 'iso3c') # Converts country name to ISO Alpha
+  #     
+  #     gadmFileName <- paste0("gadm36_", inputISO, "_1_sp.rds")  # name of the .rds file
+  # 
+  #     gadmFolder <- "gadm/" # .rds files should be stored in local gadm/ folder
+  # 
+  #     # if (file.exists(paste0(gadmFolder, gadmFileName)))
+  #     # {
+  #         Level1Identifier <- readRDS(paste0(gadmFolder, gadmFileName))
+  #     # }
+  #     # else
+  #     # {
+  #     #   Level1Identifier <- getData("GADM", level = 1, country = inputISOLower)
+  #     # }
+  #     #print(coordinates(Level1Identifier)) # coords of the region
+  #     #print(Level1Identifier$NAME_1) # List of all states/provinces/regions
+  #     
+  #     seedNames <- Level1Identifier$NAME_1
+  #     seedCoords <- coordinates(Level1Identifier)
+  #     #print(seedCoords)
+  #     seedVaxx <- c(0)
+  #     seedExpo <- c(0)
+  #     seedInfect <- c(0)
+  #     seedRec <- c(0)
+  #     seedDead <- c(0)
+  #     seedCombine <- cbind(seedNames, seedCoords, seedVaxx, seedExpo, seedInfect, seedRec, seedDead)
+  #     frameCombine <- data.frame(seedCombine)
+  # 
+  #     frameCombine <- frameCombine[c("seedNames", "V3", "V2", "seedVaxx", "seedExpo", "seedInfect", "seedRec", "seedDead")]
+  #     
+  #     colnames(frameCombine) <- c("Location", "lat", "lon", "InitialVaccinated", "InitialExposed", "InitialInfections", "InitialRecovered", "InitialDead")
+  #     #print(frameCombine)
+  #     
+  #     isoCode <- countrycode(input$selectedCountry, origin = "country.name", destination = "iso3c")
+  #     sheetName <- sprintf("%s_initialSeedData", isoCode)
+  #     
+  #     output$downloadData <- downloadHandler(
+  #       filename = function() {
+  #         paste(sheetName, Sys.Date(), ".csv",  sep = "")
+  #       },
+  #       content = function(sheetName) {
+  #         write.csv(frameCombine, sheetName, row.names = FALSE)
+  #       }
+  #     )
+  #   }
+  # })
 
   ########################################
   #Selected State/Province Map Tab Panel #
@@ -352,13 +352,13 @@ server <- function(input, output, session){
     showTab(inputId = 'tabSet', target = 'Selected State/Province Map')
   })
 
-  observe(
-    hideTab(inputId = 'tabSet', target = 'Population Count by State/Province')
-  )
-  
-  observeEvent(input$table,{
-    showTab(inputId = 'tabSet', target = 'Population Count by State/Province')
-  })
+  # observe(
+  #   hideTab(inputId = 'tabSet', target = 'Population Count by State/Province')
+  # )
+  # 
+  # observeEvent(input$table,{
+  #   showTab(inputId = 'tabSet', target = 'Population Count by State/Province')
+  # })
 }
 
 shinyApp(ui,server)
