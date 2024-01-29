dcd7a963f0bcb1b4f3797d875c287bbfb7f4bf27
Michael Myer <m-myer@live.com>
2024-01-29 09:35:51 -0700

    clipped raster error fix

--- WorldPopViz.R
+++ WorldPopViz.R
@@ -85,7 +85,7 @@ ui <- fluidPage(
                                                    
                                                    leafletOutput("leafletMap",
                                                                  width = 1024, 
-                                                                 height = 768)
+                                                                 height = 768),
                                                  ),
                                                  
 
@@ -364,6 +364,7 @@ server <- function(input, output, session){
             # if(!is.null(input$level1List)){
               #createClippedRaster(selectedCountry = input$selectedCountry, level1Region = input$level1List, rasterAgg = 0, directOutput = FALSE)
               png(outfile, width = 1024, height = 768)
+              req(!is.null(input$level1List))
               isolate(createClippedRaster(selectedCountry = input$selectedCountry, level1Region = input$level1List, susceptible()$Susceptible, directOutput = TRUE)) # Why is rasterAgg set to 0?
               dev.off()
             # }
