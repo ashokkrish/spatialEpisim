17daa718911d8321dc75d8e02159a104982926c1
Michael Myer <m-myer@live.com>
2024-02-24 02:11:07 -0700

    Updated y-axis limit
    Y-axis will now always include 0

--- R/makePlots.R
+++ R/makePlots.R
@@ -1,11 +1,14 @@
 makePlot <- function (compartments, input, plotTitle, xTitle, yTitle, lineThickness) {
+  
   compColors <- data.frame(row.names = c("S", "V", "E", "I", "R", "D"), val=c("yellow", "blue", "orange", "red", "green", "black"))
+  
   return(renderImage({
     outfile <- tempfile(fileext = '.png')
-    
     png(outfile, width = 800, height = 600)
     df <- as.data.frame(read_xlsx(paste0("www/MP4/", countrycode(input$selectedCountry, "country.name", "iso3c"), "_summary.xlsx")))
+    
     plotData = data.frame(Date = ymd(df[,"Date"]))
+    
     for (comp in compartments){
       plotData[comp] <- df[,comp]
     }
@@ -14,10 +17,13 @@ makePlot <- function (compartments, input, plotTitle, xTitle, yTitle, lineThickn
     
     p = ggplot(plotData) +
       labs(title = plotTitle, x = xTitle, y = yTitle) +
-      scale_x_date(date_labels = "%d %b %Y")
+      scale_x_date(date_labels = "%d %b %Y") +
+      ylim(0, NA)
+    
     for (comp in compartments) {
       p <- p + geom_line(aes(.data[["Date"]], .data[[comp]]), linewidth=lineThickness, color=compColors[comp,])
     }
+    
     plot(p)
     dev.off()
     
