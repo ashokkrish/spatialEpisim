4cd60aefc1ff22e6c61ddde249dc78cae3dedc69
Michael Myer <m-myer@live.com>
2024-03-15 17:34:41 -0600

    Added titles, font sizes, etc.

--- R/makeLollipop.R
+++ R/makeLollipop.R
@@ -4,26 +4,41 @@ library(readxl)
 
 plotLolliChart <- function(filename) {
   
-  data <- data.frame(
-    x = colnames(data),
-    y = colSums(data)
-  )
+  file <- as.data.frame(openDataFile(filename))
+  data <- file[,3:ncol(file)]
+  chartData <- data.frame(x = colnames(data),
+                          y = colSums(data))
   
-  # plot <- ggplot(newData, 
-  #                aes(Count, HealthZone, color = "Red")) +
-  #         geom_segment(aes(x = Count, y = HealthZone, xend = Count, yend = HealthZone), color = "grey50") +
-  #         geom_point()
+  plotTitle <- paste("Total Observed Persons per Health Zone from", file[1,"Date"], "to", file[nrow(file), "Date"])
   
-  plot <- ggplot(data, aes(x=x, y=y)) +
+  plot <- ggplot(chartData, aes(x=x, y=y)) +
     geom_segment( aes(x=x, xend=x, y=0, yend=y), color="black") +
-    geom_point( color="#18536F", size=3) +
+    geom_point( color="#18536F", size=3, position = position_dodge2(0.5)) +
+    labs(title = plotTitle,
+         x = "Health Zone", # x and y labels will be flipped
+         y = "Total Observed Persons") +
     theme_light() +
     coord_flip() +
     theme(
+      plot.title = element_text(size = 28, 
+                                face = "bold", 
+                                hjust = 0.5,
+                                margin = margin(0, 0, 50, 0)),
       panel.grid.major.y = element_blank(),
       panel.border = element_blank(),
+      plot.margin = unit(c(1, 1, 1, 1),"cm"),
+      axis.title.x = element_text(size = 20, 
+                                  face = "bold", 
+                                  vjust = -1.5,
+                                  margin = margin(25, 0, 0, 0)),
+      axis.title.y = element_text(size = 20, 
+                                  face = "bold",
+                                  margin = margin(0, 25, 0, 0)),
+      axis.text.x.bottom = element_text(size = 14),
+      axis.text.y.left = element_text(size = 14),
       axis.ticks.y = element_blank()
-    )
+    ) +
+    scale_y_continuous(n.breaks = 8)
 
   plot
 }
@@ -40,4 +55,4 @@ openDataFile <- function(filename) {
   )
 }
 
-plotLolliChart("C:/Users/Cupcake/Documents/MRU/Year 4/Winter/Senior Project/spatialEpisim/observeddata/Ebola_Death_Data.xlsx")
+# plotLolliChart("observeddata/Ebola_Incidence_Data.xlsx")
