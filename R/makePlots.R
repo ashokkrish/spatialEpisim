52cb2a98eb4c4d4281a9745b6a9bef5fd29d7b86
Michael Myer <m-myer@live.com>
2024-03-25 11:00:12 -0600

    Added legend

--- R/makePlots.R
+++ R/makePlots.R
@@ -12,33 +12,52 @@ makePlot <- function (compartments, selectedCountry, plotTitle, xTitle, yTitle, 
       plotData[comp] <- df[,comp]
     }
     
+    plotData <- as.data.frame(pivot_longer(plotData, compartments, names_to = "Compartment", values_to = "Count"))    
+    plotData$Compartment <- factor(plotData$Compartment, levels = compartments)
+    
     # , mapping = aes_string("Date", compartments[1], group = 1)
     
-    p = ggplot(plotData) +
+    p = ggplot(plotData,
+               aes(x = Date, y = Count, color = Compartment)) +
+      geom_line(linewidth = lineThickness) +
       labs(title = plotTitle, 
            x = xTitle, 
-           y = yTitle) +
+           y = yTitle,
+           color = "") +
       scale_x_date(date_labels = "%d %b %Y") +
       theme(
-        plot.title = element_text(size = 20, 
+        plot.title = element_text(size = 18, 
                                   face = "bold",
-                                  margin = margin(0, 0, 25, 0)),
-        axis.title.x = element_text(size = 18, 
+                                  margin = margin(0, 0, 25, 0),
+                                  hjust = 0.5),
+        axis.title.x = element_text(size = 14, 
                                     face = "bold",
                                     margin = margin(25, 0, 0, 0)),
-        axis.title.y = element_text(size = 18, 
+        axis.title.y = element_text(size = 14, 
                                     face = "bold",
                                     margin = margin(0, 25, 0, 0)),
         axis.text.x.bottom = element_text(size = 14),
         axis.text.y.left = element_text(size = 14),
         axis.line = element_line(linewidth = 0.5),
-        plot.margin = unit(c(1, 1, 1, 0),"cm")
+        plot.margin = unit(c(1, 1, 1, 0),"cm"),
+        legend.title = element_text(size = 10,
+                                    face = "bold"),
+        legend.box = "horizontal"
+      ) +
+      scale_color_manual(
+        values = c(
+          "S" = "yellow",
+          "V" = "blue",
+          "E" = "orange",
+          "I" = "red",
+          "R" = "green",
+          "D" = "black"
+        )
       ) +
       coord_cartesian(clip="off")
-      # ylim(0, NA)
-    
-    for (comp in compartments) {
-      p <- p + geom_line(aes(.data[["Date"]], .data[[comp]]), linewidth=lineThickness, color=compColors[comp,])
+      
+    if(length(compartments) == 1) {
+      p <- p + theme( legend.position = 'none')
     }
     
     plot(p)
