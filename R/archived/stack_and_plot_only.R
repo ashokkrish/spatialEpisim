3adc373dccd0633e524c01dcfb8fe8d2351848ab
Bryce Carson <bcars268@mtroyal.ca>
2024-06-18 17:14:06 -0600

    Delete unused files

--- R/archived/stack_and_plot_only.R
+++
@@ -1,7 +0,0 @@
-# country <- "Nigeria"
-
-# source("R/rasterStack.R")  # This code generates the base RasterStack
-# rasterStack <- createRasterStack("Nigeria", 10)
-
-# source("R/rasterPlot.R")  # This code generates a single image for RasterStack
-# printStackLayer(rasterStack = rasterStack, rasterLayer = "Susceptible", selectedCountry = "Nigeria", fname = "Susceptible.png")
