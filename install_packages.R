ea74f9c70e6ccef31c2f90a8edffcaa772c3ae55
Bryce Carson <bcars268@mtroyal.ca>
2024-06-18 10:21:52 -0600

    Silence package startup messages and ensure cloud.r-project is used.

--- install_packages.R
+++ install_packages.R
@@ -1,23 +1,25 @@
 # To run the code presented in this short-course you will need to have installed the following packages:
-
-# ipak function: install and load multiple R packages.
-# Check to see if packages are installed. Install them if they are not, then load them into the R session.
 # Be sure to update Java to its newest version
+packages = c("rsconnect",  "av",          "bslib",     "countrycode",   "cptcity",
+             "deSolve",    "dplyr",       "DT",        "ggplot2",       "here",
+             "htmltools",  "latex2exp",   "lattice",   "latticeExtra",  "leaflet",
+             "maps",       "markdown",    "plotly",    "purrr",         "rasterVis",
+             "readr",      "readxl",      "sf",        "shiny",         "shinyalert",
+             "shinybusy",  "shinyhelper", "shinyjs",   "shinyvalidate", "shinyWidgets",
+             "sp",         "stringr",     "terra",     "tidyverse",     "tinytex",
+             "tools",      "writexl",     "fasterize", "magick",        "raster",
+             "rstudioapi", "Matrix",      "lubridate", "plot3D",        "colourpicker",
+             "ggspatial")
 
-ipak <- function(pkg){
-  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
-  if (length(new.pkg)) 
-    install.packages(new.pkg, dependencies = TRUE)
-  sapply(pkg, require, character.only = TRUE)
-}
+packagesNotInstalled <- packages[!(packages %in% installed.packages()[, "Package"])]
+if (length(packagesNotInstalled))
+  install.packages(packagesNotInstalled, dependencies = TRUE, repos = "https://cloud.r-project.org")
 
-packages <- c("av", "bslib", "colorRamps", "countrycode", "cptcity", "deSolve", "dplyr", "DT", "fasterize", "ggplot2", "ggmap", "ggspatial", "htmltools", "latex2exp", "lattice", "latticeExtra", "leaflet", "lubridate", "magick", "maps", "markdown", "ncdf4",
-              "plotly", "purrr", "raster", "rasterVis", "Rcpp", "readr", "readxl", "rgdal", "rjson", "rosm", "rsconnect", "rstudioapi", "sp", "sf", "shiny", "shinyalert", "shinybusy", "shinyjs", "shinyhelper", "shinyWidgets", "stringr", "shinyFeedback",
-              "shinyvalidate", "terra", "tidyverse", "tinytex", "viridis", "writexl")
-ipak(packages)
+suppressMessages(
+  suppressPackageStartupMessages(
+    sapply(packages, require, character.only = TRUE, simplify = TRUE)
+  )
+)
 
-# To see if the needed packages are installed correctly
-lapply(packages, require, character.only = TRUE)
-
-#It is worth keeping your packages and R version up-to-date. To ensure the former, run:
-update.packages(oldPkgs = packages)
+# It is worth keeping your packages and R version up-to-date. To ensure the former, run:
+suppressWarnings(update.packages(oldPkgs = packages, repos = "https://cloud.r-project.org"))
