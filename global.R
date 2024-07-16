76136b16ffd13e9074e67c02a842f5ae23a4f769
Bryce Carson <bcars268@mtroyal.ca>
2024-07-16 00:25:29 -0600

    Clean global.R of dirty and spaghetti code
    `source` is removed; it should never be used in production application code,
    especially when Shiny already loads everything in the R/ subfolder.
    
    `suppressPackageStartupMessages` is misused; this is corrected to use an
    expression so that it is called once.
    
    _here_ is used to setup the use of `here` in the rest of the application.
    
    New commentary on various dirty codes are included.
    
    Code, dirty or not, is restyled for readability and maintainability.
    
    A call to `shinyAppDir` is included to ensure the application starts when the
    global.R file is sourced in various editors (VS Code, Positron, Emacs, VIM, etc)
    other than RStudio.

--- global.R
+++ global.R
@@ -1,64 +1,55 @@
-options(conflicts.policy = list(warn = FALSE),
-        shiny.reactlog = TRUE)
-shhh <- suppressPackageStartupMessages # It's a library, so shhh!
-shhh(library(av))
-shhh(library(bslib))
-shhh(library(cptcity))
-shhh(library(countrycode))
-shhh(library(deSolve))
-shhh(library(dplyr))
-shhh(library(DT))
-shhh(library(ggplot2))
-shhh(library(htmltools))
-shhh(library(latex2exp))
-shhh(library(lattice))
-shhh(library(latticeExtra))
-shhh(library(leaflet))
-shhh(library(maps))
-shhh(library(markdown))
-shhh(library(plotly))
-shhh(library(purrr))
-
 ## MAYBE TODO: https://rgdal.r-forge.r-project.org/reference/showWKT.html.
 ## Investigate the consequences of this option and document why it is set. Why
 ## are warnings on exportToProj4 disabled? Why were warnings encountered
 ## (presumably) when this option was originally set?
-options("rgdal_show_exportToProj4_warnings"="none")
+options("rgdal_show_exportToProj4_warnings" = "none",
+        conflicts.policy = list(warn = FALSE),
+        scipen = 999)
 
-shhh(library(rasterVis))
-shhh(library(readr))
-shhh(library(readxl))
-shhh(library(writexl))
-shhh(library(sf))     # classes and functions for vector data
-shhh(library(shiny))
-shhh(library(shinyalert))
-shhh(library(shinybusy))
-shhh(library(shinyhelper))
-shhh(library(shinyjs))
-shhh(library(shinyvalidate))
-shhh(library(shinyWidgets))
-shhh(library(sp))
-shhh(library(stringr))
-shhh(library(terra, warn.conflicts=FALSE))  # suppressWarnings(suppressMessages(library(terra)))
-shhh(library(tidyverse))
-shhh(library(tinytex))
-shhh(library(here))
+## MAYBE FIXME: I have a strong feeling not all of these libraries are actually
+## used in spatialEpisim.
+suppressPackageStartupMessages({
+  library(av)
+  library(bslib)
+  library(cptcity)
+  library(countrycode)
+  library(deSolve)
+  library(DT)
+  library(htmltools)
+
+  library(lattice)
+  library(latticeExtra)
+
+  library(leaflet)
+  library(maps)
+
+  library(markdown)
+
+  library(plotly)
+  library(rasterVis)
+
+  library(readxl)
+  library(writexl)
+
+  library(sf)
+  library(sp)
+  library(terra)
+
+  library(shiny)
+  library(shinyalert)
+  library(shinybusy)
+  library(shinyhelper)
+  library(shinyjs)
+  library(shinyvalidate)
+  library(shinyWidgets)
+
+  library(tidyverse)
+  library(here)
+
+  library(tools)
+})
 
 here::i_am("global.R")
-
-source("R/cropBaseRasterHaxby.R")
-source("R/makePlots.R")
-source("R/plotOptionsMenu.R")
-source("R/rasterBasePlot.R")
-source("R/rasterCropSeedPlot.R")
-source("R/rasterCumulativePlot.R")
-source("R/rasterLeafletPlots.R")
-#source("R/rasterSimulation.R")
-source("R/rasterSimulation_DA.R")
-source("R/rasterStack.R")
-source("R/WorldPopPlots.R")
-
-options(scipen = 999)
 
 epiparms <- read_excel("misc/epiparms.xlsx", 1)
 
@@ -68,41 +59,24 @@ prependList <- c("Czech Republic",
                  "Gambia",
                  "Netherlands")
 
+## TODO: Why are we creating our own palett when many others already exist? Use
+## an existing one which is loaded from a package we're already using. Don't
+## include a new package though, unless the palette is _really_ pretty and
+## useful.
 valueRange <- c(0, 5, 10, 25, 50, 100, 250, 1000, 10000)
-ramp <- c("#FFFFFF",
-          "#D0D8FB",
-          "#BAC5F7",
-          "#8FA1F1",
-          "#617AEC",
-          "#0027E0",
-          "#1965F0",
-          "#0C81F8",
-          "#18AFFF",
-          "#31BEFF",
-          "#43CAFF",
-          "#60E1F0",
-          "#69EBE1",
-          "#7BEBC8",
-          "#8AECAE",
-          "#ACF5A8",
-          "#CDFFA2",
-          "#DFF58D",
-          "#F0EC78",
-          "#F7D767",
-          "#FFBD56",
-          "#FFA044",
-          "#EE4F4D")
-pal <- colorRampPalette(ramp)
-colorPalette <- colorBin(pal(9)[-1], domain = valueRange, bins = valueRange)
-
-highlightDrop <- \(menu) tagList(menu, span(class = "dropDown"))
+colourPalette <-
+  colorBin((colorRampPalette(c("#FFFFFF", "#D0D8FB", "#BAC5F7", "#8FA1F1",
+                               "#617AEC", "#0027E0", "#1965F0", "#0C81F8",
+                               "#18AFFF", "#31BEFF", "#43CAFF", "#60E1F0",
+                               "#69EBE1", "#7BEBC8", "#8AECAE", "#ACF5A8",
+                               "#CDFFA2", "#DFF58D", "#F0EC78", "#F7D767",
+                               "#FFBD56", "#FFA044", "#EE4F4D")))(9)[-1],
+           domain = valueRange,
+           bins = length(valueRange))
 
 ## MAYBE TODO: replace with read_table?
 openDataFile <- function(datafile) {
-  ext <- tools::file_ext(datafile$name)
-  ext <- tolower(ext)
-
-  switch(ext,
+  switch(tolower(tools::file_ext(datafile$name)),
          csv = read_csv(datafile$datapath, show_col_types = FALSE),
          xls = read_xls(datafile$datapath),
          xlsx = read_xlsx(datafile$datapath),
@@ -110,13 +84,6 @@ openDataFile <- function(datafile) {
 
          validate("Improper file format."))
 }
-
-## TODO: move to the CSS file in www/.
-appCSS <-
-  list(".mandatory_star { color: red; }",
-       ".invisible { display:none; }",
-       ".dropDown:hover { color:ADD8E6; background-color: #000000; }") |>
-  reduce(\(...) paste(..., sep = ""))
 
 acceptedFileTypes <- c("text/csv",
                        "text/comma-separated-values",
@@ -126,6 +93,9 @@ acceptedFileTypes <- c("text/csv",
                        ".xlsx",
                        ".txt")
 
+## NOTE: borrowed from Episim; the features I use—validated inputs, inputs
+## updated with rows of a dataframe filtered on some input, et cetera—should be
+## made into a package that both use and which others can use.
 updateNumericInputs <- function(defaults, session) {
   if (any(is.null(dim(defaults)), dim(defaults)[1] != 1)) {
     warning("The `defaults` dataframe is not a single row!")
@@ -136,4 +106,8 @@ updateNumericInputs <- function(defaults, session) {
   })
 }
 
+## TODO: population is filtered by shortlist == TRUE in both ui.R and
+## WorldPopViz.R; that work should only be done once.
 population <- read_xlsx("misc/population.xlsx")
+
+shinyAppDir(here::here()) # and hurrah!
