323836d8804eabf221e4f39636aecdeb687fd752
Bryce Carson <bcars268@mtroyal.ca>
2024-08-12 21:30:42 -0600

    Cleanup the HQHT plot file

--- R/plotting/plotHQHt.R
+++ R/plotting/plotHQHt.R
@@ -1,31 +1,14 @@
 plotHQHt <- function(HQHt) {
-
-  print(paste("Determinant of HQHt: ", det(HQHt)))
+  message(paste("Determinant of HQHt =", det(HQHt)))
 
   x <- 1:ncol(HQHt)
   y <- 1:nrow(HQHt)
-  
+
   # Create a grid of x and y values
   X <- matrix(x, nrow = nrow(HQHt), ncol = ncol(HQHt), byrow = TRUE)
   Y <- matrix(y, nrow = nrow(HQHt), ncol = ncol(HQHt), byrow = FALSE)
-  
-  library(plot3D)
-  
-  ## colour function
-  # mycols <- colorRampPalette(c("blue", "lightgreen", "green", "yellow","red"))
-  
-  persp3D(x = X, y = Y, z = HQHt, theta = 90, expand = 0.5,
-          xlab = "Columns", ylab = "Rows", main = "Plot of HQHt", scale = FALSE, #col = mycols(256), clim=c(0, 1),
-          colkey = list(side = 1))
-  
-  # # Plot the 3D surface
-  # library(rgl)
-  # persp3d(X, Y, HQHt, col = "lightgreen", xlab = "Columns", ylab = "Rows", #zlab = "Values",
-  #         ticktype = "detailed", main = "3D Surface Plot of HQHt", xlim = c(0, nrow(HQHt)), ylim = c(0, ncol(HQHt)))
+
+  plot3D::persp3D(x = X, y = Y, z = HQHt, theta = 90, expand = 0.5,
+                  xlab = "Columns", ylab = "Rows", main = "Plot of HQHt", scale = FALSE,
+                  colkey = list(side = 1))
 }
-
-#--------------#
-# Example Call #
-#--------------#
-
-# plotHQHt(HQHt)
